39bae39851ae7532e40e82dad519cceab686ed71
Nyan Min Htet <austin770366@gmail.com>
2025-11-17 23:17:40 +0630

    feat:Comment show method implemenation

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostApiController;
+use App\Http\Controllers\Api\CommentApiController;
 use App\Http\Controllers\Api\ProfilePostApiController;
 
 Route::post('/v1/auth/login', [AuthController::class, 'login']);
 
 Route::prefix('v1/')->group(function (){
+
     Route::middleware(['auth:sanctum', "role.check:Author"])->group(function () {
     Route::apiResource('/authors/posts', PostApiController::class)->only('index', 'show');
-    Route::apiResource('/authors/profiles/posts', ProfilePostApiController::class)->except( 'show');
+    Route::apiResource('/authors/posts/comments', CommentApiController::class)->except('index');
+    Route::apiResource('/authors/profiles/posts', ProfilePostApiController::class)->except('show');
+    
 });
 });
 
