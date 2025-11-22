ba7edaf3b5b1253cd25f0b62f5dbbc3ffb6419de
Nyan Min Htet <austin770366@gmail.com>
2025-11-22 20:22:51 +0630

    feat: admin show api

--- app/Http/Controllers/Api/Admin/AuthorController.php
+++ app/Http/Controllers/Api/Admin/AuthorController.php
@@ -25,10 +25,11 @@ class AuthorController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(User $author)
     {
-        //
-    }
+        $author->load('posts');
+        return $this->successResponse(content: new AuthorResource($author));
+    } 
 
     /**
      * Update the specified resource in storage.
