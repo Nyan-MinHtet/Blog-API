67ddfc7d67dc78c9cdb3666fc13172ec62bb72bb
Nyan Min Htet <austin770366@gmail.com>
2025-11-22 21:22:01 +0630

    fix: migration user update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['Admin' , 'Author']);
-            $table->string('suspended')->default(false);
+            $table->integer('suspended')->default(false);
             $table->string('refresh_token')->nullable();
             $table->timestamp('refresh_token_expires_at')->nullable();
             $table->rememberToken();
