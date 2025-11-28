862dc8fd778b37a75a4a2d79044b42e85bff15b2
Nyan Min Htet <austin770366@gmail.com>
2025-11-29 00:56:05 +0630

    feat: remove destroy method from AdminProfileController

--- app/Http/Controllers/Api/Admin/ProfileController.php
+++ app/Http/Controllers/Api/Admin/ProfileController.php
@@ -15,13 +15,6 @@ class ProfileController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -35,14 +28,6 @@ class ProfileController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
     {
         //
     }
